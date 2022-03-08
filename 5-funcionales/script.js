9d2974c68d44b466b8c85e2e2d9482d428530dc3
Amilcar J. Ruiz <98772743+AmilcarJRuiz@users.noreply.github.com>
2022-03-08 18:47:12 +0100

    resueltos

--- 5-funcionales/script.js
+++ 5-funcionales/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 // puntuaciones
+
 const puntuaciones = [
   {
     equipo: "Toros Negros",
@@ -39,3 +40,30 @@ const puntuaciones = [
     puntos: [2, 3, 3, 4],
   },
 ];
+//comentarios
+//copia del objeto con los puntos sumados
+//hacer una funcion de maximo y minimo
+
+const getPoints = (pointsArray) => {
+  if (Array.isArray(pointsArray)) {
+    return pointsArray.reduce((acc, curr) => {
+      return acc + curr;
+    }, 0);
+  } else {
+    return pointsArray;
+  }
+};
+
+const getMinMax = (puntuaciones) => {
+  const orderPoints = puntuaciones.sort((a, b) => {
+    a.puntos = getPoints(a.puntos);
+    b.puntos = getPoints(b.puntos);
+
+    return b.puntos - a.puntos;
+  });
+  return {
+    first: orderPoints[0],
+    last: orderPoints[orderPoints.length - 1],
+  };
+};
+console.log(getMinMax(puntuaciones));
